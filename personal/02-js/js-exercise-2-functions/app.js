1541c867e96f1b231066897a24efd5ee3d4654b5
Grzegorz Berlinski <mrbrlnr@gmail.com>
2021-09-22 00:23:11 +0200

    Night exercises part3

--- personal/02-js/js-exercise-2-functions/app.js
+++ personal/02-js/js-exercise-2-functions/app.js
@@ -8,11 +8,18 @@
 //      return console.log('second');
 //  }
 
-let someFunc = (x, y) => {
-    return result = x + y;
-}
+//  let someFunc = (x, y) => {
+//      return result = x + y;
+//  }
 
 //someFunc(2,3) - returns 5
+
+
+let someFunc = (x, y) => {    //todo - check types of function params
+    return result = x + y + typeof result; // result is var, let or const?, EDIT: typeof returns undefined
+    
+}
+
 
 
 //hoisting
