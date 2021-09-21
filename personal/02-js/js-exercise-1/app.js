0e1f93eae21817719bde6db4d353f98f86c3d18b
Grzegorz Berlinski <mrbrlnr@gmail.com>
2021-09-21 23:49:36 +0200

    Night exercises part1

---
+++ personal/02-js/js-exercise-1/app.js
@@ -0,0 +1,33 @@
+//variables
+
+//  a;
+//  b = 4; //- without use strict mode becomes global variable and window object property
+
+//  let - variable declaration
+//  var - variable declaration
+//  const - variable declaration
+
+//  let c = 2; - declared variable with assigned value
+
+
+//data types
+//  let d = 2; - number
+//  let e = 'string'; - string
+//  let f = true; - boolean
+//  let TODO - symbol
+//  let myObject = {example: 1, somevalue: 2, thrid: true}; - object
+//  let undefinedVariable; - undefined
+//  let nullVariable = null; not null, error from the past
+//typeof d - it checks the type of d variable
+
+
+//operators
+//  let a, b, c; - comma
+//  let a = b - assignment
+//  let a += b - assignment
+//  let a *= b - assignment
+//  let a <<= b - assignment
+//  let a &= b - assignment
+//  let a ^= b - assignment
+//  console.log("Some condition " + (isMember ? "2" : "10")); - condition
+//  x || y - return true even if one is true
